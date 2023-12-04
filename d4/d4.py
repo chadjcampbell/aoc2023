65f3fc7c82fb4e93f91b6dff82aeffbe2faed202
chadjcampbell <chadjcampbell@gmail.com>
2023-12-04 18:06:53 -0500

    part 1 done

--- d4/d4.py
+++ d4/d4.py
@@ -1,6 +1,24 @@
 def main():
-    with open('test4.txt') as file:
-        ...
+    def part1():
+        with open('real4.txt') as file:
+            total = 0
+            for line in file:
+                score = 0
+                _, nums = line.split(':')
+                win_nums, card_nums = nums.split('|')
+                winners = win_nums.split()
+                card = card_nums.split()
+                for win in winners:
+                    if win in card:
+                        if score == 0:
+                            score += 1
+                        else:
+                            score *= 2
+                total += score
+
+        print(f'Part 1: {total}')
+
+    part1()
 
 
 if __name__ == '__main__':
