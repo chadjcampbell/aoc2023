acd8077d6ed901710de6f97435b14d6b6d377f9c
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-03 19:25:05 -0500

    d3 part1 done

--- d3/d3.py
+++ d3/d3.py
@@ -1,59 +1,43 @@
+import re
+
+
 def main():
 
     def part1():
-        #326901 is too low
-        #530537 too low
-        #885057 wrong
 
         def is_symbol(c):
             return not c.isdigit() and c != '.'
 
-
-        def get_num(coords, matrix):
-            num = ''
-            row,col = coords
-            while True:
-                if matrix[row][col-1].isdigit():
-                    col-=1
-                else:
-                    break
-            while True:
-                num += matrix[row][col]
-                if matrix[row][col+1].isdigit():
-                    col+=1
-                    continue
-                else:
-                    return num
+        def check_part(num, matrix, row, end):
+            start = end - len(num) + 1
+            print(num)
+            for i in range(3):
+                for j in range(len(num)+2):
+                    if -1+i+row >= 0 and -1+i+row < len(matrix) and -1+j+start >= 0 and -1+j+start < len(matrix[0]):
+                        print(matrix[-1+i+row][-1+j+start])
+                        if is_symbol(matrix[-1+i+row][-1+j+start]):
+                            print('ding!')
+                            return True
+            return False
 
         with open('d3real.txt') as file:
             matrix = [[column for column in rows.strip()] for rows in file]
-            coords = []
             nums = []
-            for i in range(len(matrix)):
-                for j in range(len(matrix[0])):
-                    if matrix[i][j].isdigit():
-                        coords.append([i,j])
-            skips = 0
-            for c in coords:
-                if skips:
-                    skips-=1
-                    continue
-                adj = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-                for a in adj:
-                    try:
-                        if is_symbol(matrix[c[0]+a[0]][c[1]+a[1]]):
-                            nums.append(int(get_num(c, matrix)))
-                            itter=1
-                            while matrix[c[0]][c[1]+itter].isdigit() and c[1]<len(matrix[0]):
-                                skips+=1
-                                itter+=1 
-                            break
-                    except IndexError:
+            for row_num, row in enumerate(matrix):
+                num = ''
+                for idx, col in enumerate(row):
+                    if col.isdigit():
+                        num += col
+                    else:
+                        if num.isdigit() and check_part(num, matrix, row_num, idx-1):
+                            nums.append(int(num))
+                        num = ''
                         continue
-            print(sum(nums))
+                    # have to check numbers at the end of the line!
+                    if col.isdigit() and num.isdigit() and check_part(num, matrix, row_num, idx-1) and idx == len(row)-1:
+                        nums.append(int(num))
 
-                    
-                    
+            print(f'Part 1: {sum(nums)}')
 
     def part2():
         with open('d3test.txt') as file:
