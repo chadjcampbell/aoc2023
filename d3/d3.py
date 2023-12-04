091dddbef444438a0d30e1661821a8f98746c02b
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-03 21:07:19 -0500

    d3, stuck on part2

--- d3/d3.py
+++ d3/d3.py
@@ -1,6 +1,3 @@
-import re
-
-
 def main():
 
     def part1():
@@ -10,13 +7,10 @@ def main():
 
         def check_part(num, matrix, row, end):
             start = end - len(num) + 1
-            print(num)
             for i in range(3):
                 for j in range(len(num)+2):
                     if -1+i+row >= 0 and -1+i+row < len(matrix) and -1+j+start >= 0 and -1+j+start < len(matrix[0]):
-                        print(matrix[-1+i+row][-1+j+start])
                         if is_symbol(matrix[-1+i+row][-1+j+start]):
-                            print('ding!')
                             return True
             return False
 
@@ -40,9 +34,45 @@ def main():
             print(f'Part 1: {sum(nums)}')
 
     def part2():
-        with open('d3test.txt') as file:
-            for line in file:
-                ...
+        # 83245878 too low
+        # ANSWER SHOULD BE 83279367
+
+        def build_num(row, col, matrix):
+            num = ''
+            while True:
+                if col-1 >= 0 and matrix[row][col-1].isdigit():
+                    col -= 1
+                else:
+                    break
+            while True:
+                num += matrix[row][col]
+                if col+1 < len(matrix[0]) and matrix[row][col+1].isdigit():
+                    col += 1
+                else:
+                    return int(num)
+
+        def check_part(row, col, matrix):
+            nums = []
+            adj = [[-1, -1], [-1, 0], [-1, 1],
+                   [0, -1], [0, 1],
+                   [1, -1], [1, 0], [1, 1]]
+            for a in adj:
+                if matrix[row+a[0]][col+a[1]].isdigit():
+                    num = build_num(row+a[0], col+a[1], matrix)
+                    if num not in nums:
+                        nums.append(num)
+            return nums
+
+        with open('d3real.txt') as file:
+            matrix = [[column for column in rows.strip()] for rows in file]
+            nums = []
+            for row_num, row in enumerate(matrix):
+                for col_num, col in enumerate(row):
+                    if col == '*':
+                        parts = check_part(row_num, col_num, matrix)
+                        if len(parts) == 2:
+                            nums.append(parts[0]*parts[1])
+            print(f'Part 2: {sum(nums)}')
 
     part1()
     part2()
