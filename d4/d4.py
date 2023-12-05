0dcffcf7cced7360c88033c77273424af93f4048
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-04 22:46:53 -0500

    d4 done

--- d4/d4.py
+++ d4/d4.py
@@ -18,7 +18,32 @@ def main():
 
         print(f'Part 1: {total}')
 
+    def part2():
+
+        def get_wins(winners, card):
+            win_count = 0
+            for win in winners:
+                if win in card:
+                    win_count += 1
+            return win_count
+
+        with open('real4.txt') as file:
+            data = file.readlines()
+            card_counts = [1 for _ in range(len(data))]
+            for i, line in enumerate(data):
+                _, nums = line.split(':')
+                win_nums, card_nums = nums.split('|')
+                winners = win_nums.split()
+                card = card_nums.split()
+                wins = get_wins(winners, card)
+                for _ in range(card_counts[i]):
+                    for j in range(wins):
+                        card_counts[i+j+1] += 1
+
+        print(f'Part 2: {sum(card_counts)}')
+
     part1()
+    part2()
 
 
 if __name__ == '__main__':
