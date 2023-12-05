b60445a2a3c38983fd98cf420535b01b65d9264a
chadjcampbell <chadjcampbell@gmail.com>
2023-12-05 12:22:56 -0500

    d5 start

---
+++ d5/d5.py
@@ -0,0 +1,14 @@
+def main():
+    def part1():
+        with open('test5.txt') as file:
+            ...
+
+    def part2():
+        ...
+
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
