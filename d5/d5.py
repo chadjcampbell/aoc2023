28184efe7f0b48275027769376a85f0573fedb89
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-06 12:17:09 -0500

    d5, stuck

--- d5/d5.py
+++ d5/d5.py
@@ -1,7 +1,19 @@
+import re
+
+
 def main():
     def part1():
         with open('test5.txt') as file:
-            ...
+            data = file.readlines()
+            seed_string = data[0].split()[1:]
+            seeds = [int(num) for num in seed_string]
+            print(seeds)
+            maps = []
+            map_num = 0
+            for idx, line in enumerate(data):
+                if 'seeds' not in line:
+                    if ':' in line:
+                        map += 1
 
     def part2():
         ...
