b5162b8bd7cad8ae335c71e3a93393174789ac6a
chadjcampbell <chadjcampbell@gmail.com>
2023-12-07 17:58:35 -0500

    d6 start

---
+++ d6/d6.py
@@ -0,0 +1,16 @@
+def main():
+
+    def part1():
+        with open('test6.txt') as file:
+            ...
+
+    def part2():
+        with open('test6.txt') as file:
+            ...
+
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
