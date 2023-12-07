86c5aec3844852df652e70e17f039207a01a130e
chadjcampbell <chadjcampbell@gmail.com>
2023-12-07 18:10:25 -0500

    d6 part 1 done

--- d6/d6.py
+++ d6/d6.py
@@ -1,8 +1,25 @@
 def main():
 
     def part1():
-        with open('test6.txt') as file:
-            ...
+        with open('real6.txt') as file:
+            data = file.readlines()
+            _, times_str = data[0].split(':')
+            _, dist_str = data[1].split(':')
+            times = [int(t) for t in times_str.split()]
+            dist = [int(d) for d in dist_str.split()]
+            print(times)
+            print(dist)
+            answer = 1
+            for idx, time in enumerate(times):
+                wins = 0
+                for i in range(time + 1):
+                    speed = i
+                    remaining_time = time - speed
+                    traveled = speed * remaining_time
+                    if traveled > dist[idx]:
+                        wins += 1
+                answer *= wins
+        print(f'Part 1: {answer}')
 
     def part2():
         with open('test6.txt') as file:
