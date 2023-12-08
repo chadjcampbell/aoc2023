3f3d2ad5514a3afbf133b933df7807d7f90efe2b
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-07 20:35:37 -0500

    d7 start

---
+++ d7/d7.py
@@ -0,0 +1,17 @@
+def main():
+    def part1():
+        with open('test6.txt') as file:
+            ...
+        print(f'Part 1: ')
+
+    def part2():
+        with open('test6.txt') as file:
+            ...
+        print(f'Part 2:')
+
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
