ac988f66922bc5139b4d00a47240a9e1454afb9f
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-09 16:21:52 -0500

    d8 part 2 not done

--- d8/d8.py
+++ d8/d8.py
@@ -27,9 +27,33 @@ def main():
         print(f'Part 1: {steps}')
 
     def part2():
-        with open('test8.txt') as file:
-            ...
-        print(f'Part 2: ')
+        with open('real8.txt') as file:
+            data = file.readlines()
+        directions = data[0].strip()
+        nodes = {}
+        for i in range(2, len(data)):
+            node, LR_string = data[i].strip().split('=')
+            L, R = LR_string.split(',')
+            nodes[node.strip()] = [L.strip()[1:], R.strip()[:-1]]
+        starts = []
+        for n in nodes.keys():
+            if n.endswith('A'):
+                starts.append(n)
+        steps = 0
+        for start in starts:
+            current = start
+            while not current.endswith('Z'):
+                for dir in directions:
+                    if dir == 'L':
+                        next = nodes[current][0]
+                    else:
+                        next = nodes[current][1]
+                    steps += 1
+                    current = next
+                    if current.endswith('Z'):
+                        break
+
+        print(f'Part 2: {steps}')
 
     part1()
     part2()
