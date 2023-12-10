ee908b2b03f0371fc52b770c44cc488a348ad7ad
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-10 09:40:06 -0500

    d9 stumped

--- d9/d9.py
+++ d9/d9.py
@@ -1,8 +1,15 @@
 def main():
 
     def part1():
+        history = []
         with open('test9.txt') as file:
+            for line in file:
+                strings = line.strip().split()
+                ints = [int(x) for x in strings]
+                history.append(ints)
+        for nums in history:
             ...
+        print(history)
         print(f'Part 1: ')
 
     def part2():
