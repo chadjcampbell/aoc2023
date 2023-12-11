d484cc0dceda325a91eaf2099f52e7b67e0c9c9d
chadjcampbell <chadjcampbell@gmail.com>
2023-12-11 18:16:57 -0500

    d9 done

--- d9/d9.py
+++ d9/d9.py
@@ -1,12 +1,12 @@
 def main():
 
-    def part1():
+    def all_zeros(arr):
+        for a in arr:
+            if a != 0:
+                return False
+        return True
 
-        def all_zeros(arr):
-            for a in arr:
-                if a != 0:
-                    return False
-            return True
+    def part1():
 
         def get_next(nums):
             if all_zeros(nums):
@@ -31,9 +31,28 @@ def main():
         print(f'Part 1: {sum}')
 
     def part2():
-        with open('test9.txt') as file:
-            ...
-        print(f'Part 2: ')
+
+        def get_prev(nums):
+            if all_zeros(nums):
+                return 0
+            diffs = []
+            for i in range(1, len(nums)):
+                diffs.append(nums[i]-nums[i-1])
+            nums.insert(0, nums[0] - get_prev(diffs))
+            return nums[0]
+
+        history = []
+        with open('real9.txt') as file:
+            for line in file:
+                strings = line.strip().split()
+                ints = [int(x) for x in strings]
+                history.append(ints)
+
+        sum = 0
+        for nums in history:
+            prev = get_prev(nums)
+            sum += prev
+        print(f'Part 2: {sum}')
 
     part1()
     part2()
